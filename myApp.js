d71e9871b846dea74ccc502ebfdc5bb257f4e245
Glitch (tortoiseshell-resolution) <none>
2020-01-14 17:57:32 +0000

    🍄🐢 Updated with Glitch

---
+++ myApp.js
@@ -0,0 +1,120 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+// --> 7)  Mount the Logger middleware here
+
+// --> 11)  Mount the body-parser middleware  here
+
+/** 1) Meet the node console. */
+
+/** 2) A first working Express Server */
+
+/**
+app.get("/", function(req, res) {  
+  res.send('Hello Express')
+})
+*/
+
+/** 3) Serve an HTML file */
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+/** 4) Serve static assets  */
+
+app.use(express.static(__dirname + "/public"));
+
+/** 5) serve JSON on a specific route */
+
+/**
+app.get('/json', function(req, res){
+  
+  res.json({"message": "Hello json"})
+  
+})
+*/
+
+/** 6) Use the .env file to configure the app */
+
+/**
+ app.get('/json', function(req, res){
+  let message="Hello json"; 
+  if(process.env.MESSAGE_STYLE === 'uppercase'){
+    res.json({"message": message.toUpperCase()})
+  }else {
+    res.json({"message":message})
+  } 
+})
+*/
+
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+
+/**
+app.use(function(req, res, next){
+  console.log(req.method+' '+req.path+' - '+req.ip)
+  next();
+})
+
+app.get('/json', function(req, res){
+  let message="Hello json"; 
+  if(process.env.MESSAGE_STYLE === 'uppercase'){
+    res.json({"message": message.toUpperCase()})
+  }else {
+    res.json({"message":message})
+  }
+})
+*/
+
+/** 8) Chaining middleware. A Time server */
+
+/**
+app.get("/now", function(req, res, next){
+  req.time = new Date().toString();
+  next();
+},function(req, res){
+  res.json({"time":req.time})      
+  })
+*/
+
+/** 9)  Get input from client - Route parameters */
+
+/*
+app.get("/:word/echo", (req, res) => {
+  res.json({
+    "echo":req.params.word
+  });
+});
+*/
+
+/** 10) Get input from client - Query parameters */
+// /name?first=<firstname>&last=<lastname>
+
+/*
+app.get('/name', (req, res)=>{
+  res.json({'name': req.query.first+' '+ req.query.last})
+})
+*/
+
+/** 11) Get ready for POST Requests - the `body-parser` */
+// place it before all the routes !
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/** 12) Get data form POST  */
+
+app.post("/name", function(req, res) {
+  let str = req.body.first + " " + req.body.last;
+
+  res.json({ name: str });
+});
+
+// This would be part of the basic setup of an Express app
+// but to allow FCC to run tests, the server is already active
+/** app.listen(process.env.PORT || 3000 ); */
+
+//---------- DO NOT EDIT BELOW THIS LINE --------------------
+
+module.exports = app;
